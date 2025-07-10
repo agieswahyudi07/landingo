d06f44bdb365d852c1b07a6bba3e0d89feb8184d
Ramadhan Agies Ananda Wahyudi <agieswahyudi@gmail.com>
2025-07-10 19:53:01 +0700

    feat : Create Hero Section (Unstyled)
    - use Poppins font from google api
    - update docker-compose volumes

--- src/main.js
+++ src/main.js
@@ -3,7 +3,10 @@ import './assets/main.css'
 import naive from 'naive-ui'
 import { createApp } from 'vue'
 import App from './App.vue'
+import {Hero} from '@/components/section'
+
 
 const app = createApp(App)
 app.use(naive)
+app.component('Hero', Hero)
 app.mount('#app')
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,4 +1,5 @@
 <template>
+  <Hero></Hero>
   <section class="min-h-screen flex flex-row gap-5 items-center justify-center bg-gradient-to-l from-white to-green-600">
     <h1 class="text-4xl text-white font-bold">🚀 Landing Page Starter - with vue </h1><img class=" max-w-32 w-16" src="@/assets/logo.svg" alt="vue icon">
   </section>
