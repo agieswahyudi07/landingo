fcb007fca96e9670655ae86bd692ee309299b783
Ramadhan Agies Ananda Wahyudi <agieswahyudi@gmail.com>
2025-09-24 22:18:45 +0700

    change button function and fix css viewport in hero

--- src/components/section/Hero.vue
+++ src/components/section/Hero.vue
@@ -87,12 +87,12 @@ const redirectToWhatsApp = () => {
         <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center xl:px-20 z-10" >
             <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
             <h1 ref="heroTitle" class="title-font sm:text-4xl text-3xl mb-4 text-gray-900 font-black uppercase">Landing Page Siap Pakai, 
-                <br class="hidden lg:inline-block">Untuk UMKM & Brand Lokal !
+                <br class="hidden lg:inline-block">Untuk UMKM & Brand <span class="whitespace-nowrap">Lokal !</span>
             </h1>
             <p ref="heroDesc" class="mb-8 leading-relaxed">Bangun kepercayaan dan convert penjualan lewat landing page profesional yang cepat, ringan, dan mobile-friendly.</p>
             <div ref="heroButtons" class="flex flex-col lg:flex-row justify-center gap-4 sm:gap-6">
-                <n-button type="info" size="large" color="#5356FF" round><span class=" mx-1 text-lg uppercase">promo launching <span class="font-semibold capitalize">Rp 999.000</span></span></n-button>
-                <n-button type="info" size="large" ghost round @click="redirectToWhatsApp" aria-label="Konsultasi gratis via WhatsApp untuk pembuatan landing page" >Konsultasi Gratis <Icon icon="mdi:whatsapp" class=" text-2xl ml-1"/></n-button>
+                <n-button type="info" size="large" color="#5356FF" round><span class=" mx-1 text-lg uppercase" @click="redirectToWhatsApp">promo launching <span class="font-semibold capitalize">Rp 999.000</span></span></n-button>
+                <!-- <n-button type="info" size="large" ghost round @click="redirectToWhatsApp" aria-label="Konsultasi gratis via WhatsApp untuk pembuatan landing page" >Konsultasi Gratis <Icon icon="mdi:whatsapp" class=" text-2xl ml-1"/></n-button> -->
             </div>
             </div>
             <div ref="heroImage" v-if="isDesktop" class=" hidden sm:inline-block">
